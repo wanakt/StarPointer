37207918114518d06866175edc4b7f89e51f1883
wanakt <92602096+wanakt@users.noreply.github.com>
2022-10-30 13:30:46 +0900

    Manual Select 추가

---
+++ StarPointer/Capture_Window.xaml.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Runtime.InteropServices;//마우스 제어
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace StarPointer
+{
+    /// <summary>
+    /// Capture_Window.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Capture_Window : Window
+    {
+
+        BitmapImage magnifying_BitmapImage;
+        int currentTargetXPosition = 0;
+        int currentTargetYPosition = 0;
+
+        Decimal scaleFactor = MainWindow.scaleFactor;
+
+        DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Send, Application.Current.Dispatcher);
+        Stopwatch stopwatch = new Stopwatch();//DispatcherTimer는 경과시간을 확인할 수 없음. 경과시간 확인위해서는 stopwatch를 별도로 돌려야 함
+
+        public Capture_Window()
+        {
+            InitializeComponent();
+            CapturedImage.Source = MainWindow.captured_BitmapImage;
+
+            int centerOfScreenWidth = (int)SystemParameters.PrimaryScreenWidth / 2;
+            int centerOfScreenHeight = (int)SystemParameters.PrimaryScreenHeight / 2;
+
+            using (Bitmap magnifying_Bmp = new Bitmap(Convert.ToInt32(50 * scaleFactor), Convert.ToInt32(50 * scaleFactor), System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(magnifying_Bmp))
+                {
+                    //중앙 화면을 캡춰해서 Bitmap 메모리에 저장
+                    gr.CopyFromScreen(Convert.ToInt32((centerOfScreenWidth - 25) * scaleFactor), Convert.ToInt32((centerOfScreenHeight - 25) * scaleFactor), 0, 0, magnifying_Bmp.Size);
+                }
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    magnifying_Bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    magnifying_BitmapImage = new BitmapImage();
+                    magnifying_BitmapImage.BeginInit();
+                    magnifying_BitmapImage.StreamSource = memory;
+                    magnifying_BitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    magnifying_BitmapImage.EndInit();
+                }
+                MagnifyingImage.Source = magnifying_BitmapImage;
+
+                currentTargetXPosition = centerOfScreenWidth;
+                currentTargetYPosition = centerOfScreenHeight;
+
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+        }
+
+        public void Target_Move(int xPosition, int yPosition)
+        {
+            lineTargetHorizontal.Margin = new Thickness(xPosition - 25, yPosition - 25, 0, 0);
+            lineTargetVertical.Margin = new Thickness(xPosition - 25, yPosition - 25, 0, 0);
+            rtTarget.Margin = new Thickness(xPosition - 25, yPosition - 25, 0, 0);
+        }
+
+        public void magnify(int currentTargetXPositon, int currentTargetYposition)
+        {
+            using (Bitmap magnifying_Bmp = new Bitmap(Convert.ToInt32(50 * scaleFactor), Convert.ToInt32(50 * scaleFactor), System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(magnifying_Bmp))
+                {
+                    gr.CopyFromScreen(Convert.ToInt32((currentTargetXPositon - 25) * scaleFactor), Convert.ToInt32((currentTargetYposition - 25) * scaleFactor), 0, 0, magnifying_Bmp.Size);
+                }
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    magnifying_Bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    magnifying_BitmapImage = new BitmapImage();//BeginInit를 여러차레 반복하려면, 이런식으로 새로운 개체를 만들어 주어야 함
+                    magnifying_BitmapImage.BeginInit();
+                    magnifying_BitmapImage.StreamSource = memory;
+                    magnifying_BitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    magnifying_BitmapImage.EndInit();
+                }
+                MagnifyingImage.Source = magnifying_BitmapImage;
+            }
+        }
+
+        private void Button_Click_Target_Up(object sender, MouseEventArgs e)
+        {
+            //DispatchrTimer는 경과시간 확인방법이 없음. 경과시간에 따라서 달리 작동하도록 만드려면 Stopwatch를 함께 이용해야 함
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            currentTargetYPosition -= 1;
+            Target_Move(currentTargetXPosition, currentTargetYPosition);
+
+            //timer와 stopwatch를 리셋
+            //확대 이미지 렌더링 속도가 너무 느려서, timer로 반복할 때에는 이미지 렌더링 생략함. Bitmap 이미지 처리 속도가 느린 것으로 추측됨
+            timer = new DispatcherTimer(DispatcherPriority.Send, Application.Current.Dispatcher);
+            timer.Interval = TimeSpan.FromMilliseconds(80);
+            timer.Tick += new EventHandler(Target_Up);
+
+            magnify(currentTargetXPosition, currentTargetYPosition);
+
+            timer.Start();
+        }
+
+        private void Target_Up(object sender, EventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 400 && stopwatch.ElapsedMilliseconds < 1000)
+            {
+                currentTargetYPosition -= 1;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 1000 && stopwatch.ElapsedMilliseconds < 2000)
+            {
+                currentTargetYPosition -= 3;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 2000)
+            {
+                currentTargetYPosition -= 10;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }    
+        }
+
+        private void Button_Click_Target_Down(object sender, RoutedEventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            currentTargetYPosition += 1;
+            Target_Move(currentTargetXPosition, currentTargetYPosition);
+
+            //timer와 stopwatch를 리셋
+            //확대 이미지 렌더링 속도가 너무 느려서, timer로 반복할 때에는 이미지 렌더링 생략함. Bitmap 이미지 처리 속도가 느린 것으로 추측됨
+            timer = new DispatcherTimer(DispatcherPriority.Send, Application.Current.Dispatcher);
+            timer.Interval = TimeSpan.FromMilliseconds(80);
+            timer.Tick += new EventHandler(Target_Down);
+
+            magnify(currentTargetXPosition, currentTargetYPosition);
+
+            timer.Start();
+        }
+
+        private void Target_Down(object sender, EventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 400 && stopwatch.ElapsedMilliseconds < 1000)
+            {
+                currentTargetYPosition += 1;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 1000 && stopwatch.ElapsedMilliseconds < 2000)
+            {
+                currentTargetYPosition += 3;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 2000)
+            {
+                currentTargetYPosition += 10;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+        }
+
+        private void Button_Click_Target_Left(object sender, RoutedEventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            currentTargetXPosition -= 1;
+            Target_Move(currentTargetXPosition, currentTargetYPosition);
+
+            //timer와 stopwatch를 리셋
+            //확대 이미지 렌더링 속도가 너무 느려서, timer로 반복할 때에는 이미지 렌더링 생략함. Bitmap 이미지 처리 속도가 느린 것으로 추측됨
+            timer = new DispatcherTimer(DispatcherPriority.Send, Application.Current.Dispatcher);
+            timer.Interval = TimeSpan.FromMilliseconds(80);
+            timer.Tick += new EventHandler(Target_Left);
+
+            magnify(currentTargetXPosition, currentTargetYPosition);
+
+            timer.Start();
+        }
+
+        private void Target_Left(object sender, EventArgs e)
+        {            
+
+            if (stopwatch.ElapsedMilliseconds > 400 && stopwatch.ElapsedMilliseconds < 1000)
+            {
+                currentTargetXPosition -= 1;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 1000 && stopwatch.ElapsedMilliseconds < 2000)
+            {
+                currentTargetXPosition -= 3;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 2000)
+            {
+                currentTargetXPosition -= 10;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+        }
+
+        private void Button_Click_Target_Right(object sender, RoutedEventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            currentTargetXPosition += 1;
+            Target_Move(currentTargetXPosition, currentTargetYPosition);
+
+            //timer와 stopwatch를 리셋
+            //확대 이미지 렌더링 속도가 너무 느려서, timer로 반복할 때에는 이미지 렌더링 생략함. Bitmap 이미지 처리 속도가 느린 것으로 추측됨
+            timer = new DispatcherTimer(DispatcherPriority.Send, Application.Current.Dispatcher);
+            timer.Interval = TimeSpan.FromMilliseconds(80);
+            timer.Tick += new EventHandler(Target_Right);
+
+            magnify(currentTargetXPosition, currentTargetYPosition);
+
+            timer.Start();
+        }
+
+        private void Target_Right(object sender, EventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > 400 && stopwatch.ElapsedMilliseconds < 1000)
+            {
+                currentTargetXPosition += 1;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 1000 && stopwatch.ElapsedMilliseconds < 2000)
+            {
+                currentTargetXPosition += 3;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+            else if (stopwatch.ElapsedMilliseconds >= 2000)
+            {
+                currentTargetXPosition += 10;
+                Target_Move(currentTargetXPosition, currentTargetYPosition);
+            }
+        }
+
+        private void Repetition_Stop(object sender, MouseButtonEventArgs e)
+        {
+            timer.Stop();
+            stopwatch.Stop();
+
+            magnify(currentTargetXPosition, currentTargetYPosition);
+        }
+
+        private void Button_Select(object sender, RoutedEventArgs e)
+        {
+            //WinForm의 Control들과 Class들은 물리적 해상도를 사용함. 따라서 디스플레이 비율이 100%가 아니면 scaleFactor를 곱해주어야 제 위치 표시
+            MainWindow.starXPosition = Convert.ToInt32(currentTargetXPosition * scaleFactor);
+            MainWindow.starYPosition = Convert.ToInt32(currentTargetYPosition * scaleFactor);
+
+            //반면에 WPF의 Control들은 디스플레이 해상도를 그대로 사용함
+            MainWindow.WindowXPosition = currentTargetXPosition;
+            MainWindow.WindowYPosition = currentTargetYPosition;
+
+            Close();
+        }
+
+        private void Button_Cancel(object sender, RoutedEventArgs e)
+        {
+            MainWindow.starXPosition = 0;
+            MainWindow.starYPosition = 0;
+            Close();
+        }
+    }
+}
--- StarPointer/MainWindow.xaml.cs
+++ StarPointer/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Runtime.InteropServices;//마우스 제어
 using System.Threading;
 using Microsoft.Win32;
+using System.Windows.Markup;
 
 namespace StarPointer
 {
@@ -22,12 +23,23 @@ namespace StarPointer
         int maxValue = 0;
         int maxValueXPosition = 0;
         int maxValueYPosition = 0;
-        int starXPosition = 0;
-        int starYPosition = 0;
-        int WindowXPosition = 0;
-        int WindowYPosition = 0;
-
+        
+        Boolean capturing = false;
         Boolean register = false;
+
+        Bitmap? captured_bmp;
+
+        public static BitmapImage captured_BitmapImage;
+        public static decimal scaleFactor = 0;
+        public static int starXPosition = 0;
+        public static int starYPosition = 0;
+        public static int WindowXPosition = 0;
+        public static int WindowYPosition = 0;
+
+        int physicalScreenWidth;
+        int physicalScreenHeight;
+        int logicalScreenWidth;
+        int logicalScreenHeight;
 
         [DllImport("user32.dll")]
         private static extern bool SetCursorPos(int x, int y);
@@ -46,11 +58,22 @@ namespace StarPointer
             maxData += "XPosition :        , " + "YPosition : ";
             tbData.Text = maxData;
 
+            //물리적 디스플레이 해상도
+            physicalScreenWidth = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+            physicalScreenHeight = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+            //논리적 디스플레이 해상도. 따라서 디스플레이 배율이 100%가 아닌 경우 물리적 디스플레이 해상도와 상이함
+            logicalScreenWidth = Convert.ToInt32(SystemParameters.PrimaryScreenWidth);
+            logicalScreenHeight = Convert.ToInt32(SystemParameters.PrimaryScreenHeight);
+
+            scaleFactor = (Decimal)physicalScreenWidth / (Decimal)logicalScreenWidth;
         }
 
         private void Button_AutoSelect_Click(object sender, RoutedEventArgs e)
         {
+            capturing = true;
+
             System.Windows.Shapes.Rectangle rectangle = (System.Windows.Shapes.Rectangle)this.FindName("rectStar");
+            
             if (rectangle != null)
             {
                 removeImage();
@@ -58,13 +81,6 @@ namespace StarPointer
             }
 
             maxValue = 0;
-
-            int physicalScreenWidth = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
-            int physicalScreenHeight = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
-            int logicalScreenWidth = Convert.ToInt32(SystemParameters.PrimaryScreenWidth);
-            int logicalScreenHeight = Convert.ToInt32(SystemParameters.PrimaryScreenHeight);
-
-            Decimal scaleFactor = (Decimal) physicalScreenWidth / (Decimal) logicalScreenWidth;
 
             WindowXPosition = Convert.ToInt32(Convert.ToDecimal(Application.Current.MainWindow.Left) * scaleFactor);
             WindowYPosition = Convert.ToInt32(Convert.ToDecimal(Application.Current.MainWindow.Top) * scaleFactor);
@@ -80,8 +96,8 @@ namespace StarPointer
             // 화면을 카피해서 Bitmap에 저장
             gr.CopyFromScreen(imageXPosition, imageYPosition, 0, 0, bmp.Size);
 
+            // 가장 밝은 지점을 확인
             int[,] brightnessArray = new int[imageSize, imageSize];
-
             for (int y = 0; y < imageSize; y++)
             {
                 for (int x = 0; x < imageSize; x++)
@@ -106,7 +122,6 @@ namespace StarPointer
 
             int rectangleXPosition = Convert.ToInt32(maxValueXPosition * (1 / scaleFactor)) - 5;
             int rectangleYPosition = Convert.ToInt32(maxValueYPosition * (1 / scaleFactor)) - 5;
-
 
             string maxData = "MaxValue : " + maxValue.ToString() + "\n";
             maxData += "XPosition : " + starXPosition.ToString() + ", " + "YPosition : " + starYPosition.ToString();
@@ -152,9 +167,8 @@ namespace StarPointer
             SetCursorPos(starXPosition, starYPosition);
             sendMouseDoubleClick(mousePosition);
 
-            btData.IsEnabled = true;
-
             Show();
+            capturing = false;
         }
 
         private void removeImage()
@@ -176,10 +190,138 @@ namespace StarPointer
             //dataWindow.Show();
         }
 
-        private void Window_Deactivated(object sender, EventArgs e)//다른 프로그램이 전체 화면으로 실행되는 경우에도 항상 위로 오게 설정
+        private void Window_Deactivated(object sender, EventArgs e)
         {
-            Window window = (Window)sender;
-            window.Topmost = true;
+            if (imStar.Source != null && capturing == false) removeImage();
+        }
+
+        private void Button_Manual_Select_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            physicalScreenWidth = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width);
+            physicalScreenHeight = Convert.ToInt32(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+            logicalScreenWidth = Convert.ToInt32(SystemParameters.PrimaryScreenWidth);
+            logicalScreenHeight = Convert.ToInt32(SystemParameters.PrimaryScreenHeight);
+
+            using (Bitmap bmp = new Bitmap(physicalScreenWidth, physicalScreenHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                // Bitmap 이미지 변경을 위해 Graphics 객체 생성
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    // 화면을 그대로 카피해서 Bitmap 메모리에 저장
+                    gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                }
+                
+                ////화면 밝기 조정 - 간단한지만 속도가 느림
+                //for (int y = 0; y < physicalScreenHeight; y++)
+                //{
+                //    for (int x = 0; x < physicalScreenWidth; x++)
+                //    {
+                //        Color color = bmp.GetPixel(x, y);
+
+                //        int r = Math.Min(255, color.R + 80);
+                //        int g = Math.Min(255, color.G + 80);
+                //        int b = Math.Min(255, color.B + 80);
+
+                //        color = Color.FromArgb(r, g, b);
+                //        bmp.SetPixel(x, y, color);
+                //    }
+                //}
+
+                //완성된 Bitmap 파일을 BitmapImage로 변환
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    captured_BitmapImage = new BitmapImage();
+                    captured_BitmapImage.BeginInit();
+                    captured_BitmapImage.StreamSource = memory;
+                    captured_BitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    captured_BitmapImage.EndInit();
+                }
+            }
+
+            //Capture_Window에 수정된 BitmapImage를 표시
+            Capture_Window capture_window = new Capture_Window();
+            capture_window.Width = logicalScreenWidth;
+            capture_window.Height = logicalScreenHeight;
+            capture_window.ShowDialog();
+
+            if (starXPosition == 0 && starYPosition == 0)
+            {
+                Show();
+                return;
+            }
+                
+
+            capturing = true;
+            int imageSize = Convert.ToInt32(120 * scaleFactor);
+            System.Windows.Shapes.Rectangle rectangle = (System.Windows.Shapes.Rectangle)this.FindName("rectStar");
+
+            if (rectangle != null)
+            {
+                removeImage();
+                return;
+            }
+
+            this.Left = WindowXPosition - 70;
+            this.Top = WindowYPosition - 110;
+
+            int imageXPosition = starXPosition - Convert.ToInt32(60 * scaleFactor);
+            int imageYPosition = starYPosition - Convert.ToInt32(60 * scaleFactor);
+
+            // 스케일을 고려하여 가로 120, 세로 120의 빈 이미지 파일 생성
+            using (Bitmap bmp = new Bitmap(imageSize, imageSize, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                // 화면을 카피해서 Bitmap에 저장
+                using (Graphics gr = Graphics.FromImage(bmp))
+                {
+                    gr.CopyFromScreen(imageXPosition, imageYPosition, 0, 0, bmp.Size);
+                }
+                string maxData = "MaxValue : Manual Select" + "\n";
+                maxData += "XPosition : " + starXPosition.ToString() + ", " + "YPosition : " + starYPosition.ToString();
+                tbData.Text = maxData;
+
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    bmp.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                    memory.Position = 0;
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.StreamSource = memory;
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.EndInit();
+
+                    imStar.Source = bitmapImage;
+
+                }
+            }
+
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+            rect.Name = "rectStar";
+            rect.Width = 10;
+            rect.Height = 10;
+            rect.VerticalAlignment = VerticalAlignment.Top;
+            rect.HorizontalAlignment = HorizontalAlignment.Left;
+            rect.Margin = new Thickness(55, 55, 0, 0);
+            rect.Fill = System.Windows.Media.Brushes.Transparent;
+            rect.StrokeThickness = 1;
+            rect.Stroke = System.Windows.Media.Brushes.Red;
+
+            Grid.SetRow(rect, 1);
+            Grid.SetColumn(rect, 1);
+            grImage.Children.Add(rect);
+
+            if (register == false) RegisterName("rectStar", rect);
+            register = true;
+
+            System.Drawing.Point mousePosition = new System.Drawing.Point(starXPosition, starYPosition);
+            SetCursorPos(starXPosition, starYPosition);
+            sendMouseDoubleClick(mousePosition);
+
+            Show();
+            capturing = false;
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -187,7 +329,6 @@ namespace StarPointer
             if (e.ChangedButton == MouseButton.Left)
             {
                 if (imStar.Source != null) removeImage();
-                btData.IsEnabled = false;
                 DragMove();
             }
         }
@@ -197,7 +338,7 @@ namespace StarPointer
             Application.Current.Shutdown();
         }
 
-        void sendMouseDoubleClick(System.Drawing.Point p)
+        private void sendMouseDoubleClick(System.Drawing.Point p)
         {
             mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, p.X, p.Y, 0, 0);
             Thread.Sleep(200);
